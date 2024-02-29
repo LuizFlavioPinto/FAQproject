2e867b8795d77fe32b87e06168b39f1e0c74920f
LuizFlavioPinto <luizflavio1452@gmail.com>
2024-02-29 06:15:44 -0300

    Project Complete. First public version
    HTML:
    
    1. Added the background Img
    2. Added the star icon
    
    CSS:
    
    1. Solved the text selection decoration problem
    2. Stylized the background image
    3. Added a box-shadow
    4. Added a media query to change the hover on the mobile devices
    
    JS:
    
    1. Added a logic to hide the active paragraf on the onclick of other paragraf

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-function changeVisibility (button) {
-	let divText = button.parentNode.querySelector('p')
+function changeVisibility (clickedElement) {
+	let divText = clickedElement.parentNode.querySelector('p')
 
-	divText.style.display == 'flex' ? divText.style.display = 'none': divText.style.display = 'flex'
+	/* There's going to be some times that the user is going to click on the h2, and the
+		parentNode is not going to find the button, bacause the button is out of the texts div.
+		So, it's necessary to find the parentNode of the parentNode
+	*/
+	let button = clickedElement.parentNode.querySelector('button')
+	if (button == null) button = clickedElement.parentNode.parentNode.querySelector('button')
+
+	let questionsContainer = document.querySelector('.questionsContainer')
+	let questionButtons = questionsContainer.querySelectorAll('button')
+	let questionPs = questionsContainer.querySelectorAll('p')
+	let activeButton 
+	let activeP 
+
+	for (let i = 0; i < questionButtons.length; i++){
+		if (questionButtons[i].style.backgroundColor == 'black'){
+			activeButton = questionButtons[i]
+			activeP = questionPs[i]
+		}
+	}
+
+
+	if(divText.style.display == 'flex') {
+		divText.style.display = 'none'
+		button.style.backgroundColor = "hsl(290, 100%, 50%)"
+		button.textContent = '+'
+
+	} else if (divText.style.display == 'none' || divText.style.display == '') {
+		divText.style.display = 'flex'
+		button.style.backgroundColor = 'black'
+		button.textContent = '-'
+		if(activeButton){
+			activeButton.style.backgroundColor = "hsl(290, 100%, 50%)"
+			activeButton.textContent = "+"
+			activeP.style.display = 'none'
+		}
+	}
+
 }
